2e7540eb0b08b054659c7d3fabe1586077f4cffe
Juliette Cordor <grassojames5@gmail.com>
2023-02-04 19:31:46 +1100

    updates and fixes

--- src/main/java/com/jewelexx/craftcolours/CraftColours.java
+++ src/main/java/com/jewelexx/craftcolours/CraftColours.java
@@ -1,26 +1,55 @@
 package com.jewelexx.craftcolours;
 
+/**
+ * Minecraft Colour Codes.
+ * <p>
+ * Stored as variables in order improve readability and use, and removing the
+ * requirement for referencing an external resource.
+ * <p>
+ * They are copied from https://www.digminecraft.com/lists/color_list_pc.php
+ */
 public final class CraftColours {
     public static final String DARK_RED = "§4";
+
     public static final String RED = "§c";
+
     public static final String GOLD = "§6";
+
     public static final String YELLOW = "§e";
+
     public static final String DARK_GREEN = "§2";
+
     public static final String GREEN = "§a";
+
     public static final String AQUA = "§b";
+
     public static final String DARK_AQUA = "§3";
+
     public static final String DARK_BLUE = "§1";
+
     public static final String BLUE = "§9";
+
     public static final String LIGHT_PURPLE = "§d";
+
     public static final String DARK_PURPLE = "§5";
+
     public static final String WHITE = "§f";
+
     public static final String GRAY = "§7";
+
     public static final String DARK_GRAY = "§8";
+
     public static final String BLACK = "§0";
+
     public static final String RESET = "§r";
+
     public static final String BOLD = "§l";
+
     public static final String STRIKETHROUGH = "§m";
+
     public static final String UNDERLINE = "§n";
+
     public static final String ITALIC = "§o";
+
     public static final String OBFUSCATED = "§k";
 }
